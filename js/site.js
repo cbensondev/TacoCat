091a135fa803bbaf2cd9e9bdb02c82d549915662
cbenson11 <67032200+cbenson11@users.noreply.github.com>
2021-08-21 23:18:39 -0500

    Initial Commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,59 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+"use strict";
+//Controller Function
+function getValue() {
+
+    //hide the alert pane
+    document.getElementById("alert").classList.add("invisible");
+
+    //get input value from screen
+    let inputText = document.getElementById("phraseInput").value;
+
+    //strip text down
+    let strippedText = stripText(inputText);
+
+    //run the palindrome checker
+    let palindromeText = palindromeMe(strippedText);
+
+    let tacoCat = false;
+    if (strippedText === palindromeText) {
+        tacoCat = true;
+    };
+
+    //display the result
+    displayResult(tacoCat, palindromeText);
+    
+};
+
+//Strip it down... Remove spaces, special characters, convert to lowercase.
+function stripText(inputText) {
+
+    let result = inputText.replace(/\s+/g, "");
+    result = result.replace(/[^a-zA-Z ]/g, "");
+    result = result.toLowerCase();
+
+    return result;    
+};
+
+function palindromeMe(inputText) {
+
+    let result = [];
+
+    for (let index = inputText.length-1; index >= 0; index--) {
+        result += inputText[index];
+    }
+
+    return result;
+};
+
+function displayResult(headerCheck, inputText) {
+    
+    if (headerCheck) {
+        document.getElementById("alertHeader").innerText = "Well Done! You entered a palindrome";
+    } else {
+        document.getElementById("alertHeader").innerText = "Nope! You did not enter a palindrome";
+    };
+
+    document.getElementById("message").innerText = inputText;
+
+    document.getElementById("alert").classList.remove("invisible");
 }
